ec943c7ce09fd832920d0b1a45fa046e8326eb1d
Your Name <you@example.com>
2024-08-16 01:31:05 +0200

    no focus on update qqt + added product name

--- front/src/components/CartView.vue
+++ front/src/components/CartView.vue
@@ -8,23 +8,25 @@ defineEmits<{ updateCart: [ProductId, number], validate: [] }>()
 <template>
     <div class="cart">
         <div v-for="(elem, index) in cart.elems_with_subtotal()" class="item">
-            <InputNumber :model-value="cart.elements[index].quantity"
-                @update:modelValue="(newcount: number) => $emit('updateCart', elem.product.product_id, newcount)"
-                inputId="horizontal-buttons" showButtons buttonLayout="horizontal" :step="1" fluid class="input-buttons">
-                <template #incrementbuttonicon>
-                    <span class="pi pi-plus"></span>
+            <p class="product-name">{{ elem.product.name }}</p>
+            <InputNumber :model-value="cart.elements[index].quantity" inputId="horizontal-buttons" showButtons
+                buttonLayout="horizontal" :step="1" fluid class="input-buttons" focused="false">
+                <template #incrementbutton>
+                    <Button icon="pi pi-plus" severity="secondary" class="increment"
+                        @click="(e) => { $emit('updateCart', elem.product.product_id, cart.elements[index].quantity + 1); e.preventDefault() }"></Button>
                 </template>
-                <template #decrementbuttonicon>
-                    <span v-if="elem.quantity > 1" class="pi pi-minus"></span>
-                    <span v-else class="pi pi-trash"></span>
+                <template #decrementbutton="test">
+                    <Button :icon="elem.quantity > 1 ? 'pi pi-minus' : 'pi pi-trash'" severity="secondary"
+                        class="decrement"
+                        @click="(e) => { $emit('updateCart', elem.product.product_id, cart.elements[index].quantity - 1); e.preventDefault() }"></Button>
                 </template>
             </InputNumber>
-            <p class="subtotal">{{ (cart.elems_with_subtotal()[index].subtotal / 100).toFixed(2) + " €"}}</p>
+            <p class="subtotal">{{ (cart.elems_with_subtotal()[index].subtotal / 100).toFixed(2) + " €" }}</p>
             <!-- <Button class="remove" @click="$emit('removeFromCart', elem.product.product_id)">-</Button> -->
         </div>
         <div class="button">
-            <p></p>
-            <Button class="valider" @click="$emit('validate')" :badge="'Total: ' + cart.get_total()" badge-severity="contrast" label="Valider"></Button>
+            <Button class="valider" @click="$emit('validate')" :badge="'Total: ' + cart.get_total()"
+                badge-severity="contrast" label="Valider"></Button>
         </div>
     </div>
 </template>
@@ -41,13 +43,29 @@ defineEmits<{ updateCart: [ProductId, number], validate: [] }>()
     justify-content: space-evenly;
 }
 
+.product-name {
+    text-transform: capitalize;
+}
+
 .subtotal {
     margin: auto 10px;
     text-align: center;
 }
 
-.input-buttons{
+.input-buttons {
     width: 50%;
+}
+
+.increment {
+    order: 3;
+    border-top-left-radius: 0;
+    border-bottom-left-radius: 0;
+}
+
+.decrement {
+    order: 1;
+    border-top-right-radius: 0;
+    border-bottom-right-radius: 0;
 }
 
 .item-content {
