1b1433a5d4ae0c86157b84802c038197085a1a83
eli-sauvage <elicolh@gmail.com>
2024-08-15 11:29:40 +0000

    rm certs, only http

--- front/certs/create-cert.sh
+++
@@ -1,5 +0,0 @@
-docker run -it --rm \
--v ./certs:/etc/letsencrypt \
--v ./nginx.conf:/etc/nginx/nginx.conf \
--v ./site:/var/www/html \
-certbot/certbot certonly --webroot -w /var/www/html -d eli-sauvage.eu
--- front/certs/renew-cert.sh
+++
@@ -1,4 +0,0 @@
-docker run -it --rm \
--v ./certs:/etc/letsencrypt \
--v ./nginx.conf:/etc/nginx/nginx.conf \
-certbot/certbot renew
