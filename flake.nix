ad4855cf2a2c038989c8a04140a32e97010d79d8
ES <elicolh@gmail.com>
2025-03-02 19:40:41 +0100

    added pre-commit for fmt + cargo sqlx prepare (fixes #29)

--- flake.nix
+++ flake.nix
@@ -7,23 +7,31 @@
     rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs { inherit system overlays; };
-        rustVersion = pkgs.rust-bin.stable.latest.default;
-      in {
-        devShell = pkgs.mkShell {
-          buildInputs =
-            with pkgs;[
-              (rustVersion.override { extensions = [ "rust-src" ]; }) 
-              #rust-analyzer
-              openssl
-              pkg-config
-              sqlx-cli
-              lld
-            ];
-        };
-      });
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    rust-overlay,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      overlays = [(import rust-overlay)];
+      pkgs = import nixpkgs {inherit system overlays;};
+      rustVersion = pkgs.rust-bin.stable.latest.default;
+    in {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          (rustVersion.override {extensions = ["rust-src"];})
+          #rust-analyzer
+          openssl
+          pkg-config
+          sqlx-cli
+          lld
+          pre-commit
+        ];
+        shellHook = ''
+          pre-commit install
+        '';
+      };
+    });
 }
